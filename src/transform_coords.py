03778de58a710aa2907f22e1e336eb57688d2573
Iryna Hurova <iryna.gurova@gmail.com>
2024-05-07 16:41:14 +0300

    updated transform coords

--- src/transform_coords.py
+++ src/transform_coords.py
@@ -4,6 +4,17 @@ import rospy
 import tf
 from find_object_2d.msg import ObjectsStamped
 from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import String
+
+def get_object_name(obj):
+    objects = {
+        "object_54": "fork",
+        "object_46": "gear",
+    }
+    if obj in list(objects.keys()):
+        return objects[obj]
+    return obj
+
 
 class TfExample:
     def __init__(self):
@@ -11,8 +22,10 @@ class TfExample:
         self.targetFrameId = rospy.get_param("~target_frame_id", "")
         self.objFramePrefix = rospy.get_param("~object_prefix", self.objFramePrefix)
         self.subs = rospy.Subscriber("objectsStamped", ObjectsStamped, self.objectsDetectedCallback)
-        self.pose_pub = rospy.Publisher('/pose_stamped', PoseStamped, queue_size=10)
+        self.pose_pub = rospy.Publisher('/object_pose', PoseStamped, queue_size=10)
+        self.name_pub = rospy.Publisher('/object_name', String, queue_size=10)
         self.tf_listener = tf.TransformListener()
+        self.tf_brodcaster = tf.TransformBroadcaster()
 
     def objectsDetectedCallback(self, msg):
         if msg.objects:
@@ -39,9 +52,11 @@ class TfExample:
                     continue
 
                 rospy.loginfo("{} [x,y,z] [x,y,z,w] in '{}' frame: [{},{},{}] [{},{},{},{}]".format(
-                    objectFrameId, targetFrameId,
+                    get_object_name(objectFrameId), targetFrameId,
                     pose[0][0], pose[0][1], pose[0][2],
                     pose[1][0], pose[1][1], pose[1][2], pose[1][3]))
+                
+                self.tf_brodcaster.sendTransform((pose[0][0], pose[0][1], pose[0][2]), (pose[1][0], pose[1][1], pose[1][2], pose[1][3]), rospy.Time.now(), get_object_name(objectFrameId), targetFrameId)
 
                 pose_stamped_msg = PoseStamped()
                 pose_stamped_msg.header.stamp = rospy.Time.now()
@@ -54,7 +69,12 @@ class TfExample:
                 pose_stamped_msg.pose.orientation.z = pose[1][2]
                 pose_stamped_msg.pose.orientation.w = pose[1][3]
 
+                name_msg = String()
+                name_msg.data = get_object_name(objectFrameId)
+
                 self.pose_pub.publish(pose_stamped_msg)
+                self.name_pub.publish(name_msg)
+
 
 if __name__ == '__main__':
     rospy.init_node('transform_coords')
